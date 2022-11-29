ff06918687e20e9a5967e9a7af20011d71d10e8f
Kilian Mader <Kilian.Mader@hotmail.de>
2022-11-29 14:00:41 +0100

    added printf statements for understanding.

--- ggT.c
+++ ggT.c
@@ -5,21 +5,33 @@ int berechne_ggT(int a, int b) {
     if (b == 0) 
         return a;
 
+    printf("a: %d ; b: %d\n",a, b);
+
     return berechne_ggT(b, a % b);
 }
 
 int main() 
 {
-    int zahlA, zahlB, ggT;
 
-    printf("Gebe eine Zahl ein: ");
-    scanf("%d", &zahlA);
+    // Variablen deklarieren
 
-    printf("Gebe eine 2. Zahl ein: ");
-    scanf("%d", &zahlB);
+        int zahlA, zahlB, ggT;
+
+    // Eingabe der Zahlen
+
+        printf("Gebe eine Zahl ein: ");
+        scanf("%d", &zahlA);
+
+        printf("Gebe eine 2. Zahl ein: ");
+        scanf("%d", &zahlB);
+
+    // Funktion zum berechnen des ggT aufrufen
       
-    ggT = berechne_ggT(zahlA, zahlB);
-    printf("%d\n", ggT);
+         ggT = berechne_ggT(zahlA, zahlB);
+
+    // Ausgabe
+
+        printf("der ggT von %d und %d lautet: %d\n", zahlA, zahlB, ggT);
      
 
     return 0;
